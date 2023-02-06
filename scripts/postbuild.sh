4da1e06a40f0d7014f632198e65ad6cbe11ab812
Bruno Paz <184563+brpaz@users.noreply.github.com>
2023-02-06 23:01:27 +0000

    fix: support for cjs module

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 # This script is executed after the build process is complete and it is used to generate a package.json file with the correct "type" field to support both ESM and CommonJS modules.
 
-echo "{ \" type\": \"module\" }" > dist/esm/package.json
+echo "{ \"type\": \"module\" }" > dist/esm/package.json
 
 echo "{ \"type\": \"commonjs\" }" > dist/cjs/package.json
