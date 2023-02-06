476038ec32e1a83ab28c58eeec61867c96ad6cf7
Bruno Paz <184563+brpaz@users.noreply.github.com>
2023-02-06 19:52:18 +0000

    chore(jest): consider only tests in src folder

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,7 @@
 import type { JestConfigWithTsJest } from 'ts-jest';
 
 const jestConfig: JestConfigWithTsJest = {
+  testMatch: ['<rootDir>/**/*.spec.ts'],
   // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
 
